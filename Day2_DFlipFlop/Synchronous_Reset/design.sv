4c98b42f8cfbbfe6172fd2b64fad27e993c9063a
Me-RajatChaudhary <111131727+Me-RajatChaudhary@users.noreply.github.com>
2022-08-31 17:35:28 +0530

    Create design.sv

---
+++ Day2_DFlipFlop/Synchronous_Reset/design.sv
@@ -0,0 +1,12 @@
+// Code your design here
+module dff_synr (
+  input logic [7:0]D,
+  input logic clk,
+  input logic reset,
+  output logic [7:0]Q
+);
+  always_ff@(posedge clk)
+  if (reset==0) Q<=D;
+  else Q<=0;
+  
+endmodule
