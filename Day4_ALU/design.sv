bb3195120b923fada8655ffff4611f9cd898fca6
Me-RajatChaudhary <111131727+Me-RajatChaudhary@users.noreply.github.com>
2022-09-10 22:37:16 +0530

    Update design.sv

--- Day4_ALU/design.sv
+++ Day4_ALU/design.sv
@@ -1,4 +1,14 @@
-module ALU;
+// Code your design here
+module day4_alu (
+  input logic [7:0]a_i;
+  input logic [7:0]b_i;
+  input logic [2:0]op;
+  output logic [7:0]alu_o
+);
   
-  //Under Progress
+  
+  typedef enum logic
+  
+  
+  
 endmodule
