effeeaf7442091ab0e7df011f64bd785a7d16096
Me-RajatChaudhary <111131727+Me-RajatChaudhary@users.noreply.github.com>
2022-09-13 19:26:09 +0530

    Update design.sv

--- Day5_OddCounter/design.sv
+++ Day5_OddCounter/design.sv
@@ -1,6 +1,19 @@
-module OddCounter (
+// Code your design here
+module odd_counter (
+  input wire clk,
+  input wire reset,
+  output logic [7:0]count_o
 );
   
+  logic [7:0] next_count;
   
+  always@(posedge clk or posedge reset) begin
+    if (reset)
+      count_o <= 8'h1;
+    else
+      count_o <= next_count;
+  end
   
+  assign next_count = count_o + 8'h2;
+        
 endmodule
