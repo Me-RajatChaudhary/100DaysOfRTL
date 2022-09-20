750a8a97f37ecade6b527d57fc6cbb31aade284d
Me-RajatChaudhary <111131727+Me-RajatChaudhary@users.noreply.github.com>
2022-09-20 16:57:52 +0530

    Create design.sv

---
+++ Day7_LFSR/design.sv
@@ -0,0 +1,20 @@
+// Code your design here
+module day7 (
+  input wire clk,
+  input wire reset,
+  output reg [3:0]lfsr_o
+);
+  
+  logic [3:0] lfsr_ff, nxt_lfsr;
+  
+  always_ff@(posedge clk or negedge reset) begin
+    if(reset)
+      lfsr_o <= 4'hE;
+    else
+      lfsr_o <= nxt_lfsr;
+  end
+  
+  assign nxt_lfsr = {lfsr_ff[2:0], lfsr_ff[1] ^ lfsr_ff[3]};
+    
+endmodule
+  
